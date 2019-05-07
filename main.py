5575eb6cae16b8edde79a42933e87d1eacd357b1
Margo <margop@margos-air.southwest.tn.edu>
2019-05-06 20:22:09 -0500

    Finished user signup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect, render_template
 import os
 import cgi
 import string
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -9,14 +10,15 @@ app.config['DEBUG'] = True
 @app.route("/signup")
 def index():
     return render_template("signup.html")
+        
 
-def spaces(text):
-    try:
-        text.index('')
-        return True
-    except ValueError:
-        return False
-
+    
+def verify_space(text):
+    spaces = False
+    for char in text:
+        if char.isspace():
+            spaces = True
+    return spaces
 
 
 
@@ -27,42 +29,42 @@ def validate_signup():
     password2= request.form['password2']
     email = request.form['email']
 
-    username_error=''
-    password_error= ''
-    password2_error = ''
-    email_error = ''
+    username_error= " "
+    password_error= " "
+    password2_error = " "
+    email_error = " "
 
 
-    if '' in username:
-        username_error='Field cannot contain blanks'
-    else: 
-        username_error = ''
-
+ 
+    if verify_space (username):       
+        username_error= "Field cannot contain blanks"
     
-    if '' in password:
-        password_error='Field cannot contain blanks'
-    else:
-        password_error = ''
+   
+    if verify_space(password):
+        password_error= "Field cannot contain blanks"
 
-    
+   
     if password != password2:
-        password2_error = 'Field does not match password'
+        password2_error = "Field does not match password"
     else:
         password2_error = ''
     
-    ###if spaces(email):
-        # email_error='Field cannot contain blanks'
-       # email=''
-    #else:
-        #if '@' and '.' not in email:
-            #email_error='Enter a valid email address'
-            #email=''
-
+    if email and verify_space(email):
+        email_error = "Please enter a valid email!"
+    if email and not re.match (r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?", email):
+        email_error= "Please enter a valid email"
     
-    if username_error or password_error or password2_error: 
-        return render_template('signup.html', username=username, password=password, password2=password2,
-        username_error=username_error, password_error=password_error, password2_error=password2_error )
+    return render_template("welcome.html", username=username)
+
+
+    if not username_error and not password_error and not password2_error and not email_error: 
+        return render_template("welcome.html", username=username)
     else:
-        return "success!"
+        return render_template('signup.html', username=username, email=email, username_error=username_error, password_error=password_error, password2_error=password2_error,
+        email_error=email_error)
+         
+    
+    
+        
 
 app.run()
